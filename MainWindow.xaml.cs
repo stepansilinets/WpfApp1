885d9995fd1bfad4a6539e8741d58c60cafb3fdd
stepansilinets <ssilinets@mail.ru>
2024-11-15 09:19:42 +0700

    kod

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -82,18 +82,7 @@ namespace WpfApp1
             }
 
           
-            string operation = parts[1];
 
-            
-            switch (operation)
-            {
-                case "+":
-                    textLabel.Text = (num1 + num2).ToString();
-                    break;
-                default:
-                    textLabel.Text = "Ошибка";
-                    break;
-            }
         }
     }
 }
